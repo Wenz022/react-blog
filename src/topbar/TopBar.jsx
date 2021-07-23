b0a11fc46e7431bd2cb01c60077e5f5c5ae86b9c
Wenz022 <satech.hjwoo@gmail.com>
2021-07-23 13:35:40 +0900

    First Commit

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.css";
+
+export default function TopBar() {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          className="topImg"
+          src="https://images.unsplash.com/photo-1504297050568-910d24c426d3?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+}
